645645936f7a6f2521cfa008b716945a3d2425bc
Artusss <volkovs.a@mail.ru>
2020-06-23 10:29:19 +0300

    Update database connection_2

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -75,8 +75,7 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
             'schema' => 'public',
-            'sslmode' => 'require',
-            'default' => env('DB_CONNECTION', 'pgsql')
+            'sslmode' => 'require'
         ],
 
         'sqlsrv' => [
