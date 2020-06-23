7147cd2fbcce4d1e60b4a995bb895fe4f609c4e5
Artusss <volkovs.a@mail.ru>
2020-06-23 11:50:39 +0300

    Add metrick seed

---
+++ database/seeds/MetricksTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class MetricksTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for($i = 0; $i < 100; $i++)
+        {
+            App\Metrick::create([
+                'site'    => 'somesite.com',
+                'clientX' => rand(0, 999),
+                'clientY' => rand(0, 999),
+                'date'    => "2020-06-23"
+                'hour'    => rand(0, 23),
+            ]);
+        }
+    }
+}
