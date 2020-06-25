9bb8b9c74e062169f451baf78bc93f2ac5a0287e
Artusss <volkovs.a@mail.ru>
2020-06-25 15:08:54 +0300

    Use other method for prepare params

--- app/Http/Controllers/MetrickController.php
+++ app/Http/Controllers/MetrickController.php
@@ -46,41 +46,11 @@ class MetrickController extends Controller
     {
         $metricks = Metrick::all();
         
-        // PREPARE PARAMS FOR GRAPH
-        $mostPickedTimePeriod_labels = array();
-        $mostPickedTimePeriod_raw    = array();
-        $mostPickedDistribution      = array();
-        for($i = 0; $i < 24; $i++)
-        {
-            $mostPickedTimePeriod_labels[] = $i;
-            $mostPickedTimePeriod_raw[$i] = array();
-        }
-        foreach($metricks as $metrick)
-        {
-            $mostPickedTimePeriod_raw[$metrick->hour][] = $metrick->id;
-            $mostPickedDistribution[] = array(
-                "X" => $metrick->clientX,
-                "Y" => $metrick->clientY
-            );
-        }
-        $mostPickedTimePeriod = array();
-        foreach($mostPickedTimePeriod_raw as $hour => $period)
-        {
-            $mostPickedTimePeriod[$hour] = count($period);
-        }
-
-        // return view("metricks.stat", array(
-        //     "mostPickedTimePeriod" => $mostPickedTimePeriod
-        // ));
-
-        // // $mostPickedTimePeriod
-        // // $mostPickedDistribution
-        // return response()->json($mostPickedTimePeriod, 200);
-
+        $prepared_params = prepareGraphParams($metricks);
 
         $metrickChart = new MetrickChart;
-        $metrickChart->labels($mostPickedTimePeriod_labels);
-        $metrickChart->dataset('Metricks by day hours (all sites)', 'line', $mostPickedTimePeriod);
+        $metrickChart->labels($prepared_params["mostPickedTimePeriod_labels"]);
+        $metrickChart->dataset('Metricks by day hours (all sites)', 'line', $prepared_params["mostPickedTimePeriod"]);
 
         return view('metricks.stat', array(
             "metrickChart" => $metrickChart
@@ -97,39 +67,46 @@ class MetrickController extends Controller
         $metricks = Metrick::where("site", $site)
             ->get();
         
-        // PREPARE PARAMS FOR GRAPH
+        $prepared_params = prepareGraphParams($metricks);
+
+        $metrickChart = new MetrickChart;
+        $metrickChart->labels($prepared_params["mostPickedTimePeriod_labels"]);
+        $metrickChart->dataset('Metricks by day hours (' . $site . ')', 'line', $prepared_params["mostPickedTimePeriod"]);
+
+        return view('metricks.stat', array(
+            "metrickChart" => $metrickChart
+        ));
+    }
+
+    // PREPARE PARAMS FOR GRAPH
+    private function prepareGraphParams($metricks)
+    {
         $mostPickedTimePeriod_labels = array();
         $mostPickedTimePeriod_raw    = array();
-        $mostPickedDistribution      = array();
+        // $mostPickedDistribution      = array();
         for($i = 0; $i < 24; $i++)
         {
-            $mostPickedTimePeriod_labels[] = $i;
+            $mostPickedTimePeriod_labels[] = count($i) < 10 ? "0{$i}:00" : "{$i}:00";
             $mostPickedTimePeriod_raw[$i] = array();
         }
         foreach($metricks as $metrick)
         {
             $mostPickedTimePeriod_raw[$metrick->hour][] = $metrick->id;
-            $mostPickedDistribution[] = array(
-                "X" => $metrick->clientX,
-                "Y" => $metrick->clientY
-            );
+            // $mostPickedDistribution[] = array(
+            //     "X" => $metrick->clientX,
+            //     "Y" => $metrick->clientY
+            // );
         }
         $mostPickedTimePeriod = array();
         foreach($mostPickedTimePeriod_raw as $hour => $period)
         {
             $mostPickedTimePeriod[$hour] = count($period);
         }
-
-        // // $mostPickedTimePeriod
-        // // $mostPickedDistribution
-
-        $metrickChart = new MetrickChart;
-        $metrickChart->labels($mostPickedTimePeriod_labels);
-        $metrickChart->dataset('Metricks by day hours (' . $site . ')', 'line', $mostPickedTimePeriod);
-
-        return view('metricks.stat', array(
-            "metrickChart" => $metrickChart
-        ));
+        return array(
+            "mostPickedTimePeriod"        => $mostPickedTimePeriod,
+            "mostPickedTimePeriod_labels" => $mostPickedTimePeriod_labels,
+            // "mostPickedDistribution"      => $mostPickedDistribution,
+        );
     }
 
     /**
